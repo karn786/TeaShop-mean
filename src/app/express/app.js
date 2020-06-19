a18cc8fd4ab9fe8567bedd11856f41e1be91e7c5
karn786 <karnkumar0701@gmail.com>
2020-06-18 22:17:07 -0700

    setup for heroku

--- src/app/express/app.js
+++ src/app/express/app.js
@@ -4,14 +4,13 @@ const app = express();
 const cors = require('cors');
 const adminRouter = require('./router/admin');
 const customerRouter = require('./router/customer');
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(adminRouter);
 app.use(customerRouter);
 
-
-
-app.listen(3000, () => {
-    console.log("server started at port 3000");
+app.listen(port, () => {
+    console.log("server started at port "+port);
 })
