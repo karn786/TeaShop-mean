dda5aeb89fd291ec9fecb304a42a756547dfc265
karn786 <karnkumar0701@gmail.com>
2020-06-18 23:06:40 -0700

    minor

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require('../src/app/mongoose/dbConnection');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const adminRouter = require('../src/app/express/router/admin');
+const customerRouter = require('../src/app/express/router/admin');
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.json());
+app.use(cors());
+app.use(adminRouter);
+app.use(customerRouter);
+app.use(express.static(__dirname + '/dist/TEASHOP'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/TEASHOP/index.html'));
+});
+
+app.listen(port, () => {
+    console.log("server started at port "+port);
+})
